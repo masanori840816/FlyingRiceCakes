bc62c2d07ae57e71a1a3775438307e5421276e4e
masanori_msl <masuimasanori@masuimasanori-no-Mac-mini.local>
2013-12-30 00:52:28 +0900

    Initial Commit

---
+++ FlyingRiceCakes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlyingRiceCakes
+//
+//  Created by masuimasanori on 2013/12/30.
+//  Copyright (c) 2013年 masanori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlyingRiceCakes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlyingRiceCakes
+//
+//  Created by masuimasanori on 2013/12/30.
+//  Copyright (c) 2013年 masanori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
